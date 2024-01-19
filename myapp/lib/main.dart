f71b75e68de8624142dcf689e3b6b3d4f2efb29a
ZP9122 <zmprajapati9122@gmail.com>
2024-01-19 15:48:44 +0530

    Splash

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/signin.dart';
 import 'package:myapp/signup.dart';
+import 'package:myapp/splash.dart';
 
 
 
