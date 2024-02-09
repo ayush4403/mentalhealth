f2ef622837ab4075223bb8f9a568968bf3e08dc0
ayush4403 <98833369+ayush4403@users.noreply.github.com>
2024-02-09 16:38:06 +0530

    final 1/2/24

--- myapp/lib/Activities/STRESSBUSTER/stressfirstscreen.dart
+++ myapp/lib/Activities/STRESSBUSTER/stressfirstscreen.dart
@@ -24,59 +24,250 @@ class MusicListScreen extends StatefulWidget {
   _MusicListScreenState createState() => _MusicListScreenState();
 }
 
-class _MusicListScreenState extends State<MusicListScreen> {
-  final List<MusicData> musicDataList = [
+class _MusicListScreenState extends State<MusicListScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  bool _animationCompleted = false;
+  late String selectedCategory;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
+      ..addListener(() {
+        setState(() {
+          _animationCompleted = true;
+        });
+      });
+    _controller.forward();
+    selectedCategory = 'Chanting';
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  late final List<MusicData> musicDataListCalm = [
     MusicData(
-      title: 'Song 1',
+      title: 'Calm 1',
       imageUrl: 'assets/Images/logos.jpg',
       audioUrl: 'Winter-Spa-chosic.mp3',
     ),
     MusicData(
-      title: 'Song 2',
+      title: 'Calm 2',
       imageUrl: 'assets/Images/logos.jpg',
       audioUrl: 'autumn-sky-meditation-7618.mp3',
     ),
-
-    // Add more music data here...
+    MusicData(
+      title: 'Calm 4',
+      imageUrl: 'assets/Images/logos.jpg',
+      audioUrl: 'autumn-sky-meditation-7618.mp3',
+    ),
+    MusicData(
+      title: 'Calm 3',
+      imageUrl: 'assets/Images/logos.jpg',
+      audioUrl: 'autumn-sky-meditation-7618.mp3',
+    ),
   ];
+  late final List<MusicData> musicDataListStressBuster = [
+    MusicData(
+      title: 'Stress 1',
+      imageUrl: 'assets/Images/logos.jpg',
+      audioUrl: 'Winter-Spa-chosic.mp3',
+    ),
+    MusicData(
+      title: 'Stress 2',
+      imageUrl: 'assets/Images/logos.jpg',
+      audioUrl: 'autumn-sky-meditation-7618.mp3',
+    ),
+  ];
+  late final List<MusicData> musicDataListChanting = [
+    MusicData(
+      title: 'Chanting 1',
+      imageUrl: 'assets/Images/logos.jpg',
+      audioUrl: 'Winter-Spa-chosic.mp3',
+    ),
+    MusicData(
+      title: 'Chanting 2',
+      imageUrl: 'assets/Images/logos.jpg',
+      audioUrl: 'autumn-sky-meditation-7618.mp3',
+    ),
+  ];
+
+  Widget _buildButtonsRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              selectedCategory = 'Chanting';
+            });
+          },
+          child: Text('Chanting'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              selectedCategory = 'Stress Buster';
+            });
+          },
+          child: Text('Stress Buster'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              selectedCategory = 'Calm';
+            });
+          },
+          child: Text('Calm'),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCategory(String categoryTitle, List<MusicData> musicList) {
+    if (selectedCategory != categoryTitle) {
+      return SizedBox.shrink();
+    }
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            categoryTitle,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 12),
+          Column(
+            children: musicList.asMap().entries.map((entry) {
+              int index = entry.key;
+              MusicData musicData = entry.value;
+              return Padding(
+                padding: EdgeInsets.only(bottom: 16),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MusicCardDataScreen(
+                          title: musicData.title,
+                          imageUrl: musicData.imageUrl,
+                          audioUrl: musicData.audioUrl,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    elevation: 3, // Apply elevation here
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width *
+                          0.9, // Adjust card width
+                      height: MediaQuery.of(context).size.height *
+                          0.16, // Adjust card height
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.asset(
+                                musicData.imageUrl,
+                                fit: BoxFit.fitHeight,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    musicData.title,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Music Card Data'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        title: Text('Music'),
       ),
-      body: ListView.builder(
-        itemCount: musicDataList.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => MusicCardDataScreen(
-                        title: musicDataList[index].title,
-                        imageUrl: musicDataList[index].imageUrl,
-                        audioUrl: musicDataList[index].audioUrl)),
-              );
-            },
-            child: Card(
-              child: ListTile(
-                leading: Image.asset(
-                  musicDataList[index].imageUrl,
-                  width: 50,
-                  height: 50,
+      body: FutureBuilder(
+        future: _controller.forward(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting ||
+              !_animationCompleted) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FadeTransition(
+                  opacity: _animation,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Choose and listen to your music',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
-                title: Text(musicDataList[index].title),
-              ),
-            ),
-          );
+                SizedBox(height: 20),
+                _buildButtonsRow(),
+                SizedBox(height: 20),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      _buildCategory('Chanting', musicDataListChanting),
+                      _buildCategory(
+                        'Stress Buster',
+                        musicDataListStressBuster,
+                      ),
+                      _buildCategory('Calm', musicDataListCalm),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }
         },
       ),
     );
--- myapp/lib/Activities/cardview.dart
+++ myapp/lib/Activities/cardview.dart
@@ -4,6 +4,7 @@ import 'package:myapp/Activities/Morning_Meditation/mindfulmeditation.dart';
 import 'package:myapp/Activities/STRESSBUSTER/stressfirstscreen.dart';
 //import 'package:myapp/Activities/quotes/daily_quote.dart';
 import 'package:myapp/Activities/quotes/daily_quotes.dart';
+//import 'package:myapp/video.dart';
 //import 'package:myapp/Activities/Stress_Buster/stress_buster.dart';
 
 class CardView extends StatelessWidget {
