e2bfa7f1c10c4c47882995f8665f7fbcbc1e33f1
ayush4403 <98833369+ayush4403@users.noreply.github.com>
2024-02-12 10:53:40 +0530

    finally

--- myapp/lib/Activities/quotes/daily_quotes.dart
+++ myapp/lib/Activities/quotes/daily_quotes.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-//import 'package:myapp/Activities/quotes/extra_quote.dart';
+import 'package:myapp/Activities/quotes/extra_quote.dart';
 import 'package:myapp/Activities/quotes/quote_fragments/person_grid.dart';
-//import 'package:myapp/Activities/quotes/random_quote.dart';
+import 'package:myapp/Activities/quotes/random_quote.dart';
 
 class DailyQuotesScreen extends StatefulWidget {
   const DailyQuotesScreen({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _DailyQuotesScreenState createState() => _DailyQuotesScreenState();
 }
 
@@ -16,6 +15,7 @@ class _DailyQuotesScreenState extends State<DailyQuotesScreen> {
 
   static final List<Widget> _fragments = <Widget>[
     const PersonScreen(),
+    RandomQuote(),
   ];
 
   void _onItemTapped(int index) {
